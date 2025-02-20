8d63dd6d0e2bf9a743b6108b4c20dc8f03b432c6
Zmore520 <128505650+Zmore520@users.noreply.github.com>
2025-02-20 16:55:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let ws = new WebSocket("wss://sose-z1oj.onrender.com/:443");
+let ws = new WebSocket("wss://socket-server-template-bixo.onrender.com:443");
 
 
 let controllTD = document.querySelector('.controllTD') ;
